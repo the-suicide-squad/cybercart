9fcbb278287fcf11dcb8895cb9db5e539397c390
pradeep800 <pradeep8b0@gmail.com>
2024-02-14 17:53:48 +0530

    error handling

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,17 +1,23 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
-import { env, initDB } from '@untitled/core'
-
+import { env, initDB } from "@untitled/core";
+import { HTTPException } from "hono/http-exception";
 const app = new Hono();
 
 async function main() {
   await initDB();
   app.get("/", (c) => {
-    return c.text("Hello Hono!");
+    return c.text("hello world");
   });
 
   console.log(`Server is running on port ${env.PORT}`);
-
+  app.onError((err, c) => {
+    if (err instanceof HTTPException) {
+      return c.json({ error: err.message }, err.status);
+    }
+    console.log(err.message);
+    return c.json({ error: "internal server error" }, 500);
+  });
   serve({
     fetch: app.fetch,
     port: env.PORT,
